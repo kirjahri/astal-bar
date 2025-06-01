48b71d943f9f950cb9c5a395c22e7791f1b2e65a
kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-05-31 22:01:42 -0400

    feat: initial commit

---
+++ widget/Bar.tsx
@@ -0,0 +1,57 @@
+import Hyprland from "gi://AstalHyprland";
+import { bind } from "astal";
+import { Gdk, Astal, App, Gtk } from "astal/gtk4";
+
+function Workspaces() {
+  const hyprland = Hyprland.get_default();
+
+  return (
+    <box>
+      {bind(hyprland, "workspaces").as((w) =>
+        w
+          .filter((w) => !(w.id >= -99 && w.id <= -2)) // filters out special workspaces
+          .sort((a, b) => a.id - b.id)
+          .map((w) => (
+            <button
+              cssClasses={bind(hyprland, "focusedWorkspace").as((f) =>
+                w === f ? ["focused"] : [""],
+              )}
+              onClicked={() => w.focus()}
+            >
+              {w.id}
+            </button>
+          )),
+      )}
+    </box>
+  );
+}
+
+function Window() {
+  const hyprland = Hyprland.get_default();
+
+  return <label label={bind(hyprland, "focusedClient").as((f) => f.title)} />;
+}
+
+export default function Bar(gdkmonitor: Gdk.Monitor) {
+  const { TOP, LEFT, RIGHT } = Astal.WindowAnchor;
+
+  return (
+    <window
+      visible
+      cssClasses={["Bar"]}
+      gdkmonitor={gdkmonitor}
+      exclusivity={Astal.Exclusivity.EXCLUSIVE}
+      anchor={TOP | LEFT | RIGHT}
+      application={App}
+    >
+      <centerbox cssName="centerbox">
+        <box hexpand halign={Gtk.Align.START}>
+          <Workspaces />
+          <Window />
+        </box>
+        <box></box>
+        <box hexpand halign={Gtk.Align.END}></box>
+      </centerbox>
+    </window>
+  );
+}
